79f2207ef8a53ab309cb861e45960d42cf8a8585
Arthur Guerreiro <arthurguerreiro36@gmail.com>
2026-02-02 00:48:43 -0300

    Create ex009_aprovacao_aluno.py

---
+++ mundo1/mundo2/ex009_aprovacao_aluno.py
@@ -0,0 +1,11 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'Média do aluno: {media}')
+
+if media >= 6:
+    print('Aluno APROVADO')
+else:
+    print('Aluno REPROVADO')
